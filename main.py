8fe07159120d6072f4048a8bbac89e821d39e5a1
isatis <515462+ladyisatis@users.noreply.github.com>
2025-10-07 18:58:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -172,8 +172,7 @@ def update():
                 try:
                     spreadsheet_html = None
 
-                    if spreadsheet_html is None:
-                    #if now.weekday() == 3 and now.hour == 0:
+                    if now.hour == 0:
                         spreadsheet_html = client.get(f"https://docs.google.com/spreadsheets/u/0/d/{ONE_PACE_EPISODE_GUIDE_ID}/htmlview/sheet?headers=true&gid={sheetId}", follow_redirects=True)
                         html_parser = BeautifulSoup(spreadsheet_html.text, "html.parser")
 
